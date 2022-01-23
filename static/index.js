18cbcf54326b2059b4e43282c7edabcd71c9ad8c
aredpea <98254676+a-Red-pea@users.noreply.github.com>
2022-01-23 19:36:04 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,11 +341,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
-    return '人？';
+    if (score <= 49) return '就吃这点？';
+    if (score <= 99) return '味道还不错吧';
+    if (score <= 149) return '厉害厉害';
+    if (score <= 199) return '别撑着了';
+    return '撑死你';
 }
 
 function toStr(obj) {
